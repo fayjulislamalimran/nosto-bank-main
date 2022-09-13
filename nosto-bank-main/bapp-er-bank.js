233f10534f47fe0623002a43067cb3328b446877
Fayjul Islam Al Imran <fayjulislamalimran7@gmail.com>
2022-09-13 10:16:26 +0600

    first commit

---
+++ nosto-bank-main/bapp-er-bank.js
@@ -0,0 +1,8 @@
+/*
+
+1.error check
+2.check others stuffs on the website
+3.check consol for error
+4.check on the link of the error (it will take you to the code)
+5.if needed add a breakpoint
+ */
